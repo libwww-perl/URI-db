32f171fbb9a44ff5dcc9b922754da9d2107ac0ea
David E. Wheeler <david@justatheory.com>
2013-12-18 09:44:03 -0800

    Create new base class _db.
    Will be moving all classes to inherit from it, including URIs with no leading
    "db:". So it does not need the code that handles the leading "db:"; that stuff
    is still in db.pm.

---
+++ lib/URI/_db.pm
@@ -0,0 +1,70 @@
+package URI::_db;
+
+use strict;
+use 5.8.1;
+use base 'URI::_login';
+our $VERSION = '0.10';
+
+sub engine { shift->scheme }
+
+sub has_recognized_engine {
+    ref $_[0] ne __PACKAGE__;
+}
+
+sub dbname {
+    my $self = shift;
+    my $is_full = $self->opaque =~ m{^//(?://|(?!/))};
+    return $self->path($is_full && defined $_[0] ? "/$_[0]" : shift) if @_;
+    my @segs = $self->path_segments or return;
+    shift @segs if $is_full;
+    join '/' => @segs;
+}
+
+sub query_params {
+    my $self = shift;
+    require URI::QueryParam;
+    return map {
+        my $f = $_;
+        map { $f => $_ } grep { defined } $self->query_param($f)
+    } $self->query_param;
+}
+
+sub _dbi_param_map {
+    my $self = shift;
+    return (
+        [ host   => scalar $self->host    ],
+        [ port   => scalar $self->_port   ],
+        [ dbname => scalar $self->dbname ],
+    );
+}
+
+sub dbi_params {
+    my $self = shift;
+    return (
+        (
+            map { @{ $_ } }
+            grep { defined $_->[1] && length $_->[1] } $self->_dbi_param_map
+        ),
+        $self->query_params,
+    );
+}
+
+sub dbi_driver { return undef }
+
+sub _dsn_params {
+    my $self = shift;
+    my @params = $self->dbi_params;
+    my @kvpairs;
+    while (@params) {
+        push @kvpairs => join '=', shift @params, shift @params;
+    }
+    return join ';' => @kvpairs;
+}
+
+sub dbi_dsn {
+    my $self = shift;
+    my $driver = $self->dbi_driver or return $self->_dsn_params;
+    return join ':' => 'dbi', $driver, $self->_dsn_params;
+}
+
+1;
--- lib/URI/db.pm
+++ lib/URI/db.pm
@@ -11,7 +11,7 @@ package URI::db;
 
 use strict;
 use 5.8.1;
-use base 'URI::_login';
+use base 'URI::_db';
 our $VERSION = '0.10';
 use overload '""' => 'as_string', fallback => 1;
 
@@ -70,68 +70,12 @@ sub engine {
 }
 
 sub has_recognized_engine {
-    ref $_[0] ne 'URI::db';
+    ref $_[0] ne __PACKAGE__;
 }
 
 sub as_string {
     my $self = shift;
     return $self->scheme . ':' . $self->SUPER::as_string(@_);
-}
-
-sub dbname {
-    my $self = shift;
-    my $is_full = $self->opaque =~ m{^//(?://|(?!/))};
-    return $self->path($is_full && defined $_[0] ? "/$_[0]" : shift) if @_;
-    my @segs = $self->path_segments or return;
-    shift @segs if $is_full;
-    join '/' => @segs;
-}
-
-sub query_params {
-    my $self = shift;
-    require URI::QueryParam;
-    return map {
-        my $f = $_;
-        map { $f => $_ } grep { defined } $self->query_param($f)
-    } $self->query_param;
-}
-
-sub _dbi_param_map {
-    my $self = shift;
-    return (
-        [ host   => scalar $self->host    ],
-        [ port   => scalar $self->_port   ],
-        [ dbname => scalar $self->dbname ],
-    );
-}
-
-sub dbi_params {
-    my $self = shift;
-    return (
-        (
-            map { @{ $_ } }
-            grep { defined $_->[1] && length $_->[1] } $self->_dbi_param_map
-        ),
-        $self->query_params,
-    );
-}
-
-sub dbi_driver { return undef }
-
-sub _dsn_params {
-    my $self = shift;
-    my @params = $self->dbi_params;
-    my @kvpairs;
-    while (@params) {
-        push @kvpairs => join '=', shift @params, shift @params;
-    }
-    return join ';' => @kvpairs;
-}
-
-sub dbi_dsn {
-    my $self = shift;
-    my $driver = $self->dbi_driver or return $self->_dsn_params;
-    return join ':' => 'dbi', $driver, $self->_dsn_params;
 }
 
 1;
