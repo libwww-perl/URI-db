87cef36e08038c9526238b5d92dc5838db19676a
Brian T. Wightman <mlx@cpan.org>
2014-08-07 10:27:41 -0500

    Add POD documentation

--- lib/URI/ldapdb.pm
+++ lib/URI/ldapdb.pm
@@ -17,3 +17,20 @@ sub dbi_dsn {
 }
 
 1;
+
+=head1 NAME
+
+URI::ldapdb
+
+=head1 DESCRIPTION
+
+Linkage between URI::db and DBD::LDAP.  I would have used
+L<URI::ldap>, but that was already taken for a module with slightly
+different intentions.
+
+=head1 CONTRIBUTED BY
+
+Brian T. Wightman <mlx@cpan.org>
+
+=cut
+ 
