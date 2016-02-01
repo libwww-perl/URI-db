05061bfc6e4e927291612d0562e08dacb77afdf2
David E. Wheeler <david@justatheory.com>
2016-02-01 09:15:15 -0800

    Switch mssql to ADO driver.

--- lib/URI/mssql.pm
+++ lib/URI/mssql.pm
@@ -1,8 +1,22 @@
 package URI::mssql;
-use base 'URI::_odbc';
+use base 'URI::_ado';
 our $VERSION = '0.17';
 
 sub default_port { 1433 }
 sub canonical_engine { 'mssql' }
+
+sub dbi_dsn {
+    my $self   = shift;
+    my $driver = shift or return $self->SUPER::dbi_dsn;
+    my $lcd    = lc $driver;
+    return $self->SUPER::dbi_dsn if $driver eq 'ado';
+
+    my $class = $lcd eq 'odbc'   ? 'URI::_odbc'
+        :       $lcd eq 'sybase' ? 'URI::sybase'
+        :       die "Unknown driver: $driver\n";
+
+    eval "require $class" or die;
+    return $class->new($self)->dbi_dsn;
+}
 
 1;
