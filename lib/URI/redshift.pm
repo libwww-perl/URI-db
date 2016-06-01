5eef1ccdbf1320e1efbc8f32718ba7a2b2c650e0
Stephen Caldwell <steve@campusexplorer.com>
2016-06-01 15:54:36 -0700

    simplify redshift class by just subclassing pg

--- lib/URI/redshift.pm
+++ lib/URI/redshift.pm
@@ -1,9 +1,5 @@
 package URI::redshift;
-use base 'URI::_db';
+use base 'URI::pg';
 our $VERSION = '0.18';
-
-sub default_port { 5432 }
-sub dbi_driver   { 'Pg' }
-sub canonical_engine { 'redshift' }
 
 1;
