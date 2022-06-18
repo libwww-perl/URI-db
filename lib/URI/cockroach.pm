b157815dec32a74b71a3be0e0588f60b0b7dd8ed
David E. Wheeler <david@justatheory.com>
2022-06-18 17:25:19 -0400

    Add CockroachDB

---
+++ lib/URI/cockroach.pm
@@ -0,0 +1,16 @@
+package URI::cockroach;
+use base 'URI::_db';
+our $VERSION = '0.20';
+
+sub default_port { 26257 }
+sub dbi_driver   { 'Pg' }
+sub canonical_engine { 'cockroach' }
+
+sub _dbi_param_map {
+    my $self = shift;
+    return (
+        [ host   => scalar $self->host   ],
+        [ port   => scalar $self->port ],
+        [ dbname => scalar $self->dbname ],
+    );
+}
