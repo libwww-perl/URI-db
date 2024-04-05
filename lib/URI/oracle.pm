bfbd9f84ee0841fc176ceb0aac473e67b5f26981
David E. Wheeler <david@justatheory.com>
2024-04-04 21:25:10 -0400

    Update Oracle database DBI parameter generation
    Again. It now works as follows:
    
    -   If there is not a hostname or port, just use the raw SID or service
        name as the database name: `dbi:Oracle:$name`. This is way 1 in the
        DBD::Oracle documentation
    -   If there is a hostname or port, use the EZCONNECT syntax. This is
        way 3 in the DBD::Oracle documentation.
    -   If there are query parameters, delimit them by `&` and not `;`.
    
    Use of a port without a host name may not be valid, but it seems most
    prudent to build an EZCONNECT that includes the port in this context and
    to let Oracle or DBD::Oracle reject it if appropriate. Thanks again to
    @vectro for the and diligence, testing, and patience with this issue
    (#22).

--- lib/URI/oracle.pm
+++ lib/URI/oracle.pm
@@ -5,13 +5,28 @@ our $VERSION = '0.22';
 sub default_port { 1521 }
 sub dbi_driver   { 'Oracle' }
 
-sub _dbi_param_map {
+sub _dsn_params {
     my $self = shift;
-    return (
-        [ host => scalar $self->host   ],
-        [ port => scalar $self->_port  ],
-        [ service_name => scalar $self->dbname ],
-    );
+    my $name = $self->dbname || '';
+    my $dsn = $self->host;
+
+    if (my $p = $self->_port) {
+        $dsn .= ":$p";
+    }
+
+    return $name unless $dsn;
+    $dsn .= "/$name";
+
+
+    if (my @p = $self->query_params) {
+        my @kvpairs;
+        while (@p) {
+            push @kvpairs => join '=', shift @p, shift @p;
+        }
+        $dsn .= '?' . join '&' => @kvpairs;
+    }
+
+    return "//$dsn";
 }
 
 1;
